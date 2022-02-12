6c1eba03f93edacea2973b19a1fa9838fae51aea
GeoffreyXue <xuegeoff@gmail.com>
2022-02-11 19:12:32 -0600

    login

--- src/api/api.js
+++ src/api/api.js
@@ -1,25 +1,19 @@
-import { useEffect } from "react";
+export default class UWAPI {
+    async static Login() {
+        let userinfo = "smurk:davis";
 
-function Login()
-{
-	let userinfo = "smurk:davis";
-	useEffect(() => {
-		authenticate();
-	},[])
-	function authenticate()
-	{
-		const str = "Basic " + btoa(userinfo)
-		const options = {
-			method: 'POST',
-			headers: {
-				'Authorization': str,
-				'Accept': application/JSON,
-				'Content-Type': application/w-xxx-form-urlencoded,
-			}
-		}
-		fetch("https://api.wisc.edu/oauth/token",options).then(response => response.json);
-	
-	
-	}
+        const str = "Basic " + btoa(userinfo)
+        const options = {
+            method: 'POST',
+            headers: {
+                'Authorization': str,
+                'Accept': application/JSON,
+                'Content-Type': application/w-xxx-form-urlencoded,
+            }
+        }
+        
+        let response =  await fetch("https://api.wisc.edu/oauth/token", options);
+        let data = response.json();
+        return data;
+    }
 }
-
--- src/routes/Login/Login.js
+++ src/routes/Login/Login.js
@@ -1,4 +1,15 @@
+import { useEffect } from 'react';
+import UWAPI from '../../api/api';
+
 export function Login() {
+
+    useEffect(() => {
+        UWAPI.Login()
+            .then(data => {
+                console.log(data);
+            })
+    }, [])
+
     return (
         <div>Login Page</div>
     )
