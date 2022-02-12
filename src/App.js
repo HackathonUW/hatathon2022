c8b2ea344830364ae7b58997d61384ffcec445b9
GeoffreyXue <xuegeoff@gmail.com>
2022-02-11 18:49:31 -0600

    navigation WIP

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
+import { Home, Login} from './routes'
+import { ChakraProvider } from '@chakra-ui/react'
+import { Navigation } from './components/Navigation';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link as RouteLink
+} from "react-router-dom";
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <ChakraProvider>
+        <Router>
+            <Navigation />
+            <Switch>
+                <Route path="/">
+                    <Home />
+                </Route>
+                <Route path="/login">
+                    <Login />
+                </Route>
+            </Switch>
+        </Router>
+      </ChakraProvider>
   );
 }
 
